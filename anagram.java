89b64327d54d1f8c828f57de9df847742418400c
Alice Huang <alice@expertise.com>
2017-07-19 14:59:52 -0700

    Java anagrams

---
+++ anagram.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static boolean isAnagram(String a, String b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        Map<String, Integer> frequency = new HashMap<String, Integer>();
+        for (int i=0; i<a.length(); i++) {
+            frequency.put(a.substring(i,i+1), 0);
+        }
+        for (int j=0; j<a.length(); j++) {
+            int n = frequency.get(a.substring(j,j+1)) + 1;
+            frequency.put(a.substring(j,j+1), n);
+        }
+        for (int i=0; i<b.length(); i++) {
+            int x;
+            if(frequency.get(b.substring(i,i+1)) != null) {
+                x = frequency.get(b.substring(i,i+1));
+                frequency.put(b.substring(i,i+1), x-1);
+            } else {
+                frequency.put(b.substring(i,i+1), 1);
+            }
+
+        }
+        List<Integer> list = new ArrayList<Integer>(frequency.values());
+        int sum = 0;
+        for (int i : list) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
