076d28d46543bcc394f05a1b36fef07526efa6e8
Alice Huang <alicehccn@gmail.com>
2019-04-27 11:57:01 -0700

    Set up input for cheapestFlights

--- cheapestFlights.js
+++ cheapestFlights.js
@@ -7,6 +7,13 @@
  * @return {number}
  https://leetcode.com/problems/cheapest-flights-within-k-stops/submissions/
  */
+
+var n = 10
+var flights = [[3,4,4],[2,5,6],[4,7,10],[9,6,5],[7,4,4],[6,2,10],[6,8,6],[7,9,4],[1,5,4],[1,0,4],[9,7,3],[7,0,5],[6,5,8],[1,7,6],[4,0,9],[5,9,1],[8,7,3],[1,2,6],[4,1,5],[5,2,4],[1,9,1],[7,8,10],[0,4,2],[7,2,8]]
+var src = 6
+var dst = 0
+var K = 7
+
 var findCheapestPrice = function(n, flights, src, dst, K) {
     var g = new Graph(n);
     for (var i = 0; i < flights.length; i++) {
@@ -63,7 +70,7 @@ class Graph {
         for (var i = 0; i < this.nodes; i++) {
             var current = this.minDistance();
             console.log(current)
-            this.visited[i] = true;
+            this.visited[current] = true;
             if (i == K+1) break;
             if (!this.adjacentList[current]) continue;
             for (var j = 0; j < this.adjacentList[current].length; j++) {
@@ -79,3 +86,6 @@ class Graph {
             return -1;
     }
 }
+
+console.log(findCheapestPrice(n, flights, src, dst, K))
+
